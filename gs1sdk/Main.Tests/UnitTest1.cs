1137038a0737d2137f79c4c915daf85ece35b304
Abdullah Ahmad Fouad <abdullahahmadfouad@gmail.com>
2023-08-15 11:17:40 +0300

    second  commit

--- gs1sdk/Main.Tests/UnitTest1.cs
+++ gs1sdk/Main.Tests/UnitTest1.cs
@@ -1,161 +1,116 @@
 using Xunit;
 using Main.Services;
+using System;
+using System.Collections.Generic;
+
 namespace Main.Tests;
 
-public class UnitTest1
+
+
+
+
+public class UnitTest1 
 {
 
+    private EPCConverter epcConverter;
 
-     private EPCConverter epcConverter;
-
-    [SetUp]
-    public void SetUp()
+    public UnitTest1()
     {
-        // common Arrange
         epcConverter = new EPCConverter();
     }
+   
 
     [Theory]
-    [InlineData(-1)]
-    [InlineData(0)]
-    [InlineData(1)]
-    public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+    [MemberData(nameof(sgtinGs1ElementToEpcDataProvider))]
+    public void testSgtinToEpc(string sgtin, string barcode, string expectedEpcUri)
     {
-        var main = new Class1();
-        bool result = main.IsPrime(1);
-
-        Assert.False(result, "${value} should not be prime");
-    }
-
-
-    public void testSgtinToEpc(String sgtin, String barcode, String expectedEpcUri)
-    {
-        String actualEpcUri = this.epcConverter.sgtinToEpc(sgtin);
-        Assert.Equals(expectedEpcUri,actualEpcUri);
+        string actualEpcUri = this.epcConverter.sgtinToEpc(sgtin);
+        Assert.Equal(expectedEpcUri,actualEpcUri);
     }
 
     /**
      * @dataProvider gtinGs1ElementToEpcDataProvider
-     * @param String gtin
-     * @param String barcode not used in this test
-     * @param String expectedEpcUri
+     * @param string gtin
+     * @param string barcode not used in this test
+     * @param string expectedEpcUri
      */
-    public void testGtinToEpc(String gtin, String barcode, String expectedEpcUri)
+    [Theory]
+    [MemberData(nameof(gtinGs1ElementToEpcDataProvider))]
+    public void testGtinToEpc(string gtin, string barcode, string expectedEpcUri)
     {
-       String actualEpcUri = this.epcConverter.gtinToEpc(gtin);
-        Assert.Equals(expectedEpcUri,actualEpcUri);
+       string actualEpcUri = this.epcConverter.gtinToEpc(gtin);
+        Assert.Equal(expectedEpcUri,actualEpcUri);
     }
 
     /**
      * @dataProvider ssccGs1ElementToEpcDataProvider
-     * @param String sscc
-     * @param String barcode
-     * @param String expectedEpcUri
+     * @param string sscc
+     * @param string barcode
+     * @param string expectedEpcUri
      */
-    public void testSsccToEpc(String sscc, String barcode, String expectedEpcUri)
+    [Theory]
+    [MemberData(nameof(ssccGs1ElementToEpcDataProvider))]
+    public void testSsccToEpc(string sscc, string barcode, string expectedEpcUri)
     {
-       String actualEpcUri = this.epcConverter.ssccToEpc(sscc);
-        Assert.Equals(expectedEpcUri,actualEpcUri);
+       string actualEpcUri = this.epcConverter.ssccToEpc(sscc);
+        Assert.Equal(expectedEpcUri,actualEpcUri);
     }
 
     /**
      * @dataProvider glnGs1ElementToEpcDataProvider
-     * @param String gln
-     * @param String extension
-     * @param String barcode
-     * @param String expectedEpcUri
+     * @param string gln
+     * @param string extension
+     * @param string barcode
+     * @param string expectedEpcUri
      */
-    public void testGlnToEpc(String gln, String extension, String barcode, String expectedEpcUri)
+    [Theory]
+    [MemberData(nameof(glnGs1ElementToEpcDataProvider))]
+    public void testGlnToEpc(string gln, string extension, string barcode, string expectedEpcUri)
     {
-       String actualEpcUri = this.epcConverter.glnToEpc(gln);
-        Assert.Equals(expectedEpcUri,actualEpcUri);
+       string actualEpcUri = this.epcConverter.glnToEpc(gln);
+        Assert.Equal(expectedEpcUri,actualEpcUri);
     }
 
 
-    public static List<Dictionary<string, string>> sgtinGs1ElementToEpcDataProvider()
-    {
-        return new List<Dictionary<string, string>>
+    
+
+    public static IEnumerable<object[]> sgtinGs1ElementToEpcDataProvider =>
+        new List<object[]>
         {
-            {
-                "sgtin"         => "06281080008450OUEY0LB818UM",
-                "elementString" => "010628108000845021OUEY0LB818UM",
-                "epcUri"        => "urn:epc:id:sgtin:6281080.000845.OUEY0LB818UM"
-            },
-            {
-                "sgtin"         => "06281080008450OUEY0LB81XYZ",
-                "elementString" => "010628108000845021OUEY0LB81XYZ",
-                "epcUri"        => "urn:epc:id:sgtin:6281080.000845.OUEY0LB81XYZ"
-            },
-            {
-                "sgtin"         => "06281080010088Ser1ex",
-                "elementString" => "010628108001008821Ser1ex",
-                "epcUri"        => "urn:epc:id:sgtin:6281080.001008.Ser1ex"
-            },
+            new object[] { "06281080008450OUEY0LB818UM", "010628108000845021OUEY0LB818UM", "urn:epc:id:sgtin:6281080.000845.OUEY0LB818UM"},
+            new object[] { "06281080008450OUEY0LB81XYZ", "010628108000845021OUEY0LB81XYZ", "urn:epc:id:sgtin:6281080.000845.OUEY0LB81XYZ"},
+            new object[] { "06281080010088Ser1ex", "010628108001008821Ser1ex", "urn:epc:id:sgtin:6281080.001008.Ser1ex" },
+            
+        };
+
+    public static IEnumerable<object[]> gtinGs1ElementToEpcDataProvider =>
+        new List<object[]>
+        {
+            new object[] { "06281080008450", "0106281080008450", "urn:epc:id:sgtin:6281080.000845.*"},
+            new object[] { "06281080008450", "0106281080008450", "urn:epc:id:sgtin:6281080.000845.*"},
+            new object[] { "06281080010088", "0106281080010088", "urn:epc:id:sgtin:6281080.001008.*" },
 
         };
-    }
 
-    public static List<Dictionary<string, string>> gtinGs1ElementToEpcDataProvider()
-    {
-        return new List<Dictionary<string, string>>
+    
+    public static IEnumerable<object[]> ssccGs1ElementToEpcDataProvider =>
+        new List<object[]>
         {
-            {
-                "gtin"          => "06281080008450",
-                "elementString" => "0106281080008450",
-                "epcUri"        => "urn:epc:id:sgtin:6281080.000845.*"
-            },
-            {
-                "gtin"          => "06281080008450",
-                "elementString" => "0106281080008450",
-                "epcUri"        => "urn:epc:id:sgtin:6281080.000845.*"
-            },
-            {
-                "gtin"          => "06281080010088",
-                "elementString" => "0106281080010088",
-                "epcUri"        => "urn:epc:id:sgtin:6281080.001008.*"
-            },
+            new object[] { "062810800000808289", "00062810800000808289", "urn:epc:id:sscc:6281080.0000080828"},
+            new object[] { "806141411234567896", "00806141411234567896", "urn:epc:id:sscc:0614141.8123456789"},
 
         };
-    }
 
 
-    public static List<Dictionary<string, string>> ssccGs1ElementToEpcDataProvider()
-    {
-        return new List<Dictionary<string, string>>
+
+    public static IEnumerable<object[]> glnGs1ElementToEpcDataProvider =>
+        new List<object[]>
         {
-            {
-                "sscc"          => "062810800000808289",
-                "elementString" => "00062810800000808289",
-                "epcUri"        => "urn:epc:id:sscc:6281080.0000080828"
-            },
-            {
-                "sscc"          => "806141411234567896",
-                "elementString" => "00806141411234567896",
-                "epcUri"        => "urn:epc:id:sscc:0614141.8123456789"
-            },
+            new object[] { "0614141123452", "xxxxx", "xxxxx", "urn:epc:id:sgln:0614141.12345.0"},
+            new object[] { "0857624006082", "xxxxx", "xxxxx", "urn:epc:id:sgln:0857624006.08.0"},
 
         };
-    }
-
-    public static List<Dictionary<string, string>> glnGs1ElementToEpcDataProvider()
-    {
-        return new List<Dictionary<string, string>>
-        {
-            {
-                "gln"           => "0614141123452",
-                "glnExtension"  => "xxxxx",
-                "elementString" => "xxxxx",
-                "epcUri"        => "urn:epc:id:sgln:0614141.12345.0"
-            },
-            {
-                "gln"           => "0857624006082",
-                "glnExtension"  => "xxxxx",
-                "elementString" => "xxxxx",
-                "epcUri"        => "urn:epc:id:sgln:0857624006.08.0"
-            },
-
-        };
-    }
+    
 
 
 
---
+++ gs1sdk/Main/EPCConverter.cs
@@ -0,0 +1,159 @@
+﻿using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http.Json;
+
+namespace Main.Services;
+public class EPCConverter
+{
+    public IDictionary<string, int> gcpLengthMapping;
+
+    
+    public const string KEY_PREFIX = "P-";
+    public EPCConverter()
+    {
+        var jsonArray = File.ReadAllText("data.json");
+        var list = JsonSerializer.Deserialize<IEnumerable<KeyValuePair<string, int>>>(jsonArray);
+        var gcpLengthMapping = list.ToDictionary(x => x.Key, x => x.Value);
+    }
+    //1-
+    public void injectAdditionalGCPLengthMappings(int [] mappings)
+    {
+
+    }
+
+    //2-
+    public void injectTestGCPPrefixMappings(int [] mappings)
+    {
+
+    }
+    //3-
+    public string version(bool print = true)
+    {
+
+    }
+    //4-
+    public string epcToSscc(string epc)
+    {
+
+    }
+    //5-
+    public string[] epcToGln(string epc)
+    {
+
+    }
+    //6-
+    public string[] epcToSgtin(string epc)
+    {
+
+    }
+    //7-
+    public string sgtinToEpc(string sgtin, int gcpLength = null)
+    {
+
+    }
+    //8-
+    public string[] sgtinArrayToEpc(string gtin, string[] serials, int gcpLength = null)
+    {
+
+    }
+    //9-
+    public string sgtinBarcodeToEpc(string sgtinBarcode, int gcpLength = null)
+    {
+
+    }
+    //10-
+    public string gtinToEpc(string gtin, int gcpLength = null)
+    {
+
+    }
+    //11-
+    public string ssccToEpc(string sscc, int gcpLength = null)
+    {
+
+    }
+    //12-
+    public string ssccBarcodeToEpc(string ssccBarcode, int gcpLength = null)
+    {
+
+    }
+    //13-
+    public string glnToEpc(string gln, int gcpLength = null)
+    {
+
+    }
+    //14-
+    public string glnAndDocNumToBizTrx(string gln, string documentNumber)
+    {
+
+    }
+    //15-
+    public int getGcpLength(string gs1Element)
+    {
+
+    }
+    //16-
+    private string[] extractGTINFragments(string sgtin, int gcpLength)
+    {
+
+    }
+    //17-
+    private void reportGcpLengthError(string gs1Element, string elementType)
+    {
+
+    }
+    //18-
+    private void reportGenericError(string errorDescription)
+    {
+
+    }
+    //19-
+    public bool hasValidCheckDigit(string gln)
+    {
+
+    }
+
+
+   
+    //20-
+    public string sgtinToEpc(string sgtin)
+    {
+        return "";
+    }
+    //21-
+    public string glnToEpc(string sgtin)
+    {
+        return "";
+    }
+        //22-
+    public string ssccToEpc(string sgtin)
+    {
+        return "";
+    }
+        //23-
+    public string gtinToEpc(string sgtin)
+    {
+        return "";
+    }       
+
+
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        EPCConverter a = new EPCConverter();
+        Console.WriteLine(a.gcpLengthMapping);
+        Console.WriteLine();
+    }
+
+}
+
+
+public class PrefixFormat
+{
+    string prefix;
+    int gcpLength;
+}
